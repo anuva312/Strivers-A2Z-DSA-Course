94d989517579e33d73d6ecfa26ca2ac1d4aeec77
Anusree V A <anusreeva93@gmail.com>
2023-07-15 18:15:11 -0700

    Added js solution to first problem in hashing

---
+++ 5. Hashing/1. Counting Frequencies/solution.js
@@ -0,0 +1,20 @@
+class Solution {
+    frequencyCount(arr,N,P){
+        const countMap = {};
+       for(const element of arr){
+           if(countMap[element]){
+               countMap[element]++;
+           }
+           else{
+               countMap[element] = 1;
+           }
+       }
+       for(let i=0;i<N;i++){
+           if(countMap[i+1]){
+               arr[i] = countMap[i+1]
+           }else{
+               arr[i]=0;
+           }
+       }
+    }
+}
