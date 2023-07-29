c6436d3cb2e301358f30f32fbe7e70e45e2413b2
Anusree V A <anusreeva93@gmail.com>
2023-07-29 22:52:23 +0530

    Added an alternative solution

--- 4. Recursion/2. Print 1 to N/solution.py
+++ 4. Recursion/2. Print 1 to N/solution.py
@@ -1,38 +1,27 @@
-#User function Template for python3
+# Time Complexity: O(N)
+# Space Complexity: O(N) (due to the call stack)
+# Auxiliary Space: O(1)
 
-class Solution:    
-    #Complete this function
+class Solution: 
     def printNos(self,N,new_val =1):
         if new_val>N:
             return
         print(new_val,end=" ")
         self.printNos(N,new_val+1)
 
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-import math
-
-
-
+    def printNosAlternative(self,N):
+        if(N>0):
+            self.printNos(N-1)
+            print(N,end=" ")
 
 def main():
-    
     T=int(input())
-    
     while(T>0):
-        
-        
         N=int(input())
-        
         ob=Solution()
-        
         ob.printNos(N)
         print()
         T-=1
 
 if __name__=="__main__":
     main()
-# } Driver Code Ends
