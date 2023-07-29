a02b396d4f0a726d18c67a55abf02c8a3882aba5
Anusree V A <anusreeva93@gmail.com>
2023-07-29 12:57:47 +0530

    added optimized solution

---
+++ 5. Hashing/1. Counting Frequencies/solution-optimized.py
@@ -0,0 +1,19 @@
+def frequencyCount(arr,N,P):
+    i=0
+    while i < N:
+        if(arr[i] <0 or arr[i]>N or arr[i]>P):
+            i+=1
+            continue
+        element_index = arr[i]-1
+        if(arr[element_index]>0):
+            arr[i] = arr[element_index]
+            arr[element_index] = -1
+        else:
+            arr[element_index]-=1
+            arr[i]=0
+            i+=1
+    for i in range(N):
+        if(arr[i]<0):
+            arr[i]*=-1
+        else:
+            arr[i]=0
