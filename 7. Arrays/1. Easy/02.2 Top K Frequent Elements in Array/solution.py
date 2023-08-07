295af13e09f7a399381722768e168cf450dcac76
Anusree V A <anusreeva93@gmail.com>
2023-08-07 11:08:33 +0530

    updated solution

--- 7. Arrays/1. Easy/02.2 Top K Frequent Elements in Array/solution.py
+++ 7. Arrays/1. Easy/02.2 Top K Frequent Elements in Array/solution.py
@@ -1,3 +1,7 @@
+# Time Complexity: O(n log n)
+# Space Complexity: O(n)
+# Auxiliary Space: O(n)
+
 def topK( nums, k):
     countMap = {}
     for element in nums:
@@ -5,7 +9,7 @@ def topK( nums, k):
             countMap[element] +=1
         else:
             countMap[element] = 1
-    sortedKeys = sorted(list(countMap.keys()),key=lambda x:(countMap[x],x),reverse=True)
+    sortedKeys = sorted(list(countMap.keys()),key=lambda x:(-countMap[x],-x))
     if(len(sortedKeys)<k):
         return sortedKeys
     return sortedKeys[:k]
