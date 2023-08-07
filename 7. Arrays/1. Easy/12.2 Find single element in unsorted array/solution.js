d60a0f099aeb79120a0d8cbdfab4babe8f7ef745
Anusree V A <anusreeva93@gmail.com>
2023-08-07 18:29:35 +0530

    fixed a mistake in the code

--- 7. Arrays/1. Easy/12.2 Find single element in unsorted array/solution.js
+++ 7. Arrays/1. Easy/12.2 Find single element in unsorted array/solution.js
@@ -1,5 +1,5 @@
 // Hint : XOR of two equal numbers is 0
-singleNumber = function (nums) {
+const singleNumber = function (nums) {
   let result = 0;
   for (const element of nums) {
     result = result ^ element;
