07994b123e7805031ed0221fbdde84daaae68914
Anusree V A <anusreeva93@gmail.com>
2023-10-14 18:20:08 -0700

    added optimized solution

--- 7. Arrays/1. Easy/10 Missing Number/solution.js
+++ 7. Arrays/1. Easy/10 Missing Number/solution.js
@@ -1,10 +1,33 @@
-const missingNumber = function (nums) {
+// Time Complexity: O(nlogn)
+// Space Complexity: O(1)
+const missingNumberBruteForce = function (nums) {
   nums.sort((a, b) => a - b);
-  console.log(nums);
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] != i) {
       return i;
     }
   }
   return nums.length;
+};
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+const missingNumber = function (nums) {
+  let numsMap = {};
+  for (const num of nums) {
+    numsMap[num] = 1;
+  }
+  for (let i = 0; i < nums.length; i++) {
+    if (!numsMap[i]) return i;
+  }
+  return nums.length;
+};
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+const missingNumberOptimized = function (nums) {
+  const arrayLength = nums.length;
+  const expectedSum = (arrayLength * (arrayLength + 1)) / 2;
+  const actualSum = nums.reduce((partialSum, a) => partialSum + a, 0);
+  return expectedSum - actualSum;
 };
