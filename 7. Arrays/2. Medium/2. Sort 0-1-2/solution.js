c4d9f85a633cc3bc66d2269c021e5ad3656c21bd
Anusree V A <anusreeva93@gmail.com>
2023-10-21 19:56:44 -0700

    added optimized solution

--- 7. Arrays/2. Medium/2. Sort 0-1-2/solution.js
+++ 7. Arrays/2. Medium/2. Sort 0-1-2/solution.js
@@ -1,4 +1,7 @@
 // Brute Force Solution
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Two Pass Approach
 
 const sortColors = function (nums) {
   let zeroesCount = 0;
@@ -19,6 +22,34 @@ const sortColors = function (nums) {
     } else if (twosCount) {
       nums[i] = 2;
       twosCount--;
+    }
+  }
+};
+
+// Dutch National Flag Algorithm
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// One Pass Approach
+
+const sortColorsOptimized = function (nums) {
+  let low = 0;
+  let mid = 0;
+  let high = nums.length - 1;
+  let temp = 0;
+  while (mid <= high) {
+    if (nums[mid] === 0) {
+      temp = nums[low];
+      nums[low] = nums[mid];
+      nums[mid] = temp;
+      low++;
+      mid++;
+    } else if (nums[mid] === 1) {
+      mid++;
+    } else {
+      temp = nums[high];
+      nums[high] = nums[mid];
+      nums[mid] = temp;
+      high--;
     }
   }
 };
